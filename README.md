a3f6ede29040e065b1a5be390cd33aa33f5639c0
Lex <zhang13pro@qq.com>
2021-09-13 18:09:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Do it myself
+
+music-online web.
+
+source repository address:
+[https://github.com/Ryleeing](https://github.com/Ryleeing/Tap_your_tap-/tree/main/Tap-your-tap%E4%BD%A0%E7%9A%84%E9%9F%B3%E4%B9%90%E4%BA%A4%E4%BA%92%E7%A9%BA%E9%97%B4)
---
+++ index.js
@@ -0,0 +1,12 @@
+const audio00 = './assets/audio/沙锤.mp3';
+const audio01 = './assets/audio/脚鼓.mp3';
+const audio02 = './assets/audio/饶钹.mp3';
+const audio10 = './assets/audio/踏钹.mp3';
+const audio11 = './assets/audio/军鼓.mp3';
+const audio12 = './assets/audio/掌声.mp3';
+const bgMusic = './assets/audio/bgmusic.mp3';
+
+const musicFragments = [
+    [audio00, audio01, audio02],
+    [audio10, audio11, audio12],  
+]
