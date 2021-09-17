deb44e498ef0ba4774ccab70bb277c00ae713d97
zhang13pro <lexzhang13@qq.com>
2021-09-17 22:55:38 +0800

    animation

--- animation.js
+++ animation.js
@@ -16,5 +16,12 @@ function getRandomColor() {
   const rgb1 = Math.floor(Math.random() * 257);
   const rgb2 = Math.floor(Math.random() * 257);
   const rgb3 = Math.floor(Math.random() * 257);
-  return rgb(rgb1, rgb2, rgb3);
+  return `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+}
+
+function animate1() {
+  const set = Snap.set();
+  const radius = getRandomNum(20, 40);
+  const num = getRandomNum(5, 15);
+  const angle = 360 / num;
 }
